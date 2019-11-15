7d9768f3fa158a935e250ef39d4eb16e9eae8721
caoshiqi <402971588@qq.com>
2019-11-15 15:58:11 +0800

    some configuration

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      'modules': false,
-      'targets': {
-        'browsers': ['> 1%', 'last 2 versions', 'not ie <= 8']
+    [
+      '@vue/app',
+      {
+        useBuiltIns: 'entry'
       }
-    }]
+    ]
   ],
   plugins: [
     '@babel/plugin-transform-runtime',
